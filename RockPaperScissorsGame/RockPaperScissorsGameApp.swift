deb097b3b44c9e9cfbafac861a9f15b5743cbcb7
abduladhil227 <abduladhil006@gmail.com>
2024-08-16 23:13:13 +0530

    Initial Commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Abdul Adhil on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
